9003724d024caf69d3e8b84cb74698f49d597bf3
justinsubmit <justinsubmit@gmail.com>
2021-05-19 17:53:39 +0800

    test

--- model/User.py
+++ model/User.py
@@ -11,117 +11,119 @@ class User:
     @classmethod
     def getUser(cls,userid):
         try:
-            dbConn=DatabasePool.getConnection()
-            #db_Info = dbConn.connection_id
-            #print(f"Connected to {db_Info}");
+            # dbConn=DatabasePool.getConnection()
+            # #db_Info = dbConn.connection_id
+            # #print(f"Connected to {db_Info}");
 
-            cursor = dbConn.cursor(dictionary=True)
-            sql="select * from user where userid=%s"
+            # cursor = dbConn.cursor(dictionary=True)
+            # sql="select * from user where userid=%s"
 
-            cursor.execute(sql,(userid,))
-            users = cursor.fetchall() 
+            # cursor.execute(sql,(userid,))
+            # users = cursor.fetchall() 
 
-            return users
+            # return users
+            return ""
 
         finally:
-            dbConn.close()
-            print("release connection")
+            pass
+            # dbConn.close()
+            # print("release connection")
 
-    @classmethod
-    def getAllUsers(cls):
-        dbConn=DatabasePool.getConnection()
-        cursor = dbConn.cursor(dictionary=True)
-        sql="select * from user"
-        cursor.execute(sql)
-        users = cursor.fetchall()
+    # @classmethod
+    # def getAllUsers(cls):
+    #     dbConn=DatabasePool.getConnection()
+    #     cursor = dbConn.cursor(dictionary=True)
+    #     sql="select * from user"
+    #     cursor.execute(sql)
+    #     users = cursor.fetchall()
 
-        dbConn.close()
+    #     dbConn.close()
 
-        return users
+    #     return users
 
-    @classmethod
-    def insertUser(cls,userJson):
-        dbConn=DatabasePool.getConnection()
-        cursor = dbConn.cursor(dictionary=True)
+    # @classmethod
+    # def insertUser(cls,userJson):
+    #     dbConn=DatabasePool.getConnection()
+    #     cursor = dbConn.cursor(dictionary=True)
 
-        # Hash a password for the first time, with a randomly-generated salt
-        # print("password1",userJson["password"])
-        # password2 = userJson["password"]
-        password=userJson["password"].encode() 
+    #     # Hash a password for the first time, with a randomly-generated salt
+    #     # print("password1",userJson["password"])
+    #     # password2 = userJson["password"]
+    #     password=userJson["password"].encode() 
         
-        # print("passwordencoded",password)
-        # #convert string to bytes
-        hashed = bcrypt.hashpw(password, bcrypt.gensalt())
-        print("hashed",hashed)
-        # print("gensalt",gensalt())
-        sql="insert into user(username,email,role,password) Values(%s,%s,%s,%s)"
-        users = cursor.execute(sql,(userJson["username"],userJson["email"],userJson["role"],hashed))
+    #     # print("passwordencoded",password)
+    #     # #convert string to bytes
+    #     hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+    #     print("hashed",hashed)
+    #     # print("gensalt",gensalt())
+    #     sql="insert into user(username,email,role,password) Values(%s,%s,%s,%s)"
+    #     users = cursor.execute(sql,(userJson["username"],userJson["email"],userJson["role"],hashed))
         
-        dbConn.commit()
-        rows=cursor.rowcount
-        #print(cursor.lastrowid)
+    #     dbConn.commit()
+    #     rows=cursor.rowcount
+    #     #print(cursor.lastrowid)
 
-        dbConn.close()
+    #     dbConn.close()
 
-        return rows
+    #     return rows
 
     
-    @classmethod
-    def updateUser(cls,userid,email,password):
-        dbConn=DatabasePool.getConnection()
-        cursor = dbConn.cursor(dictionary=True)
+    # @classmethod
+    # def updateUser(cls,userid,email,password):
+    #     dbConn=DatabasePool.getConnection()
+    #     cursor = dbConn.cursor(dictionary=True)
 
-        #password need to hash for new ver with hashing...
-        sql="update user set email=%s,password=%s where userid=%s"
-        users = cursor.execute(sql,(email,password,userid))
-        dbConn.commit()
-        rows=cursor.rowcount
+    #     #password need to hash for new ver with hashing...
+    #     sql="update user set email=%s,password=%s where userid=%s"
+    #     users = cursor.execute(sql,(email,password,userid))
+    #     dbConn.commit()
+    #     rows=cursor.rowcount
 
-        dbConn.close()
+    #     dbConn.close()
 
-        return rows
+    #     return rows
 
-    @classmethod
-    def deleteUser(cls,userid):
-        dbConn=DatabasePool.getConnection()
-        cursor = dbConn.cursor(dictionary=True)
+    # @classmethod
+    # def deleteUser(cls,userid):
+    #     dbConn=DatabasePool.getConnection()
+    #     cursor = dbConn.cursor(dictionary=True)
 
-        sql="delete from user where userid=%s"
-        users = cursor.execute(sql,(userid,))
-        dbConn.commit()
-        rows=cursor.rowcount
+    #     sql="delete from user where userid=%s"
+    #     users = cursor.execute(sql,(userid,))
+    #     dbConn.commit()
+    #     rows=cursor.rowcount
 
-        dbConn.close()
+    #     dbConn.close()
 
-        return rows
+    #     return rows
 
-    @classmethod
-    def loginUser(cls,userJSON):
-        try:
-            dbConn=DatabasePool.getConnection()
-            #db_Info = dbConn.connection_id
-            #print(f"Connected to {db_Info}");
+    # @classmethod
+    # def loginUser(cls,userJSON):
+    #     try:
+    #         dbConn=DatabasePool.getConnection()
+    #         #db_Info = dbConn.connection_id
+    #         #print(f"Connected to {db_Info}");
 
-            print(userJSON)
-            cursor = dbConn.cursor(dictionary=True)
-            sql="select * from user where email=%s"
+    #         print(userJSON)
+    #         cursor = dbConn.cursor(dictionary=True)
+    #         sql="select * from user where email=%s"
 
-            cursor.execute(sql,(userJSON["email"],))
-            user = cursor.fetchone() #at most 1 record since email is supposed to be unique
-            if user==None:
-                return {"jwt":""}
+    #         cursor.execute(sql,(userJSON["email"],))
+    #         user = cursor.fetchone() #at most 1 record since email is supposed to be unique
+    #         if user==None:
+    #             return {"jwt":""}
 
-            else:
+    #         else:
                 
-                password=userJSON["password"].encode()
-                hashed=user['password'].encode()
+    #             password=userJSON["password"].encode()
+    #             hashed=user['password'].encode()
                 
-                if bcrypt.checkpw(password, hashed):#True means valid password 
-                    payload={"userid":user["userid"],"role":user["role"],"exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=7200)}
+    #             if bcrypt.checkpw(password, hashed):#True means valid password 
+    #                 payload={"userid":user["userid"],"role":user["role"],"exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=7200)}
 
-                    jwtToken=jwt.encode(payload,Settings.secretKey,algorithm="HS256")
-                    return {"jwt":jwtToken}
-                else:
-                    return {"jwt":""}
-        finally:
-            dbConn.close()
+    #                 jwtToken=jwt.encode(payload,Settings.secretKey,algorithm="HS256")
+    #                 return {"jwt":jwtToken}
+    #             else:
+    #                 return {"jwt":""}
+    #     finally:
+    #         dbConn.close()
