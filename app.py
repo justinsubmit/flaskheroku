7fcfd1c1df330522eecc49ac21444295ee1f9022
justinsubmit <justinsubmit@gmail.com>
2021-05-19 19:37:41 +0800

    test

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ app = Flask(__name__)
 # http://localhost:5000?data=test@test.com
 @app.route('/')
 def validate():
-    host='localhost'
-    database='furniture'
-    user='root'
-    password='Singapore1'
-    # host=os.environ['HOST2']
-    # database=os.environ['DATABASE2']
-    # user=os.environ['USERNAME2']
-    # password=os.environ['PASSWORD2']
-    # print("host2",host)
+    # host='localhost'
+    # database='furniture'
+    # user='root'
+    # password='Singapore1'
+    host=os.environ['HOST2']
+    database=os.environ['DATABASE2']
+    user=os.environ['USERNAME2']
+    password=os.environ['PASSWORD2']
+    print("host2",host)
     connection_pool = pooling.MySQLConnectionPool(pool_name="ws_pool",
                                                   pool_size=5,
                                                   host=host,
@@ -35,12 +35,12 @@ def validate():
     # database=os.environ['DATABASE2']
     # user=os.environ['USERNAME2']
     # password=os.environ['PASSWORD2']
-   
+    dbConn.close()
     return "hello"
 
 
 @app.route('/users')
-def validate():
+def validate2():
     host='localhost'
     database='furniture'
     user='root'
