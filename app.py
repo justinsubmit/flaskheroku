88fb1e05ffd165f61274988f68c3d318b9d9174b
justinsubmit <justinsubmit@gmail.com>
2021-05-19 19:36:07 +0800

    test

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ app = Flask(__name__)
 # http://localhost:5000?data=test@test.com
 @app.route('/')
 def validate():
-    # host='localhost'
-    # database='furniture'
-    # user='root'
-    # password='Singapore1'
-    host=os.environ['HOST2']
-    database=os.environ['DATABASE2']
-    user=os.environ['USERNAME2']
-    password=os.environ['PASSWORD2']
-    print("host2",host)
+    host='localhost'
+    database='furniture'
+    user='root'
+    password='Singapore1'
+    # host=os.environ['HOST2']
+    # database=os.environ['DATABASE2']
+    # user=os.environ['USERNAME2']
+    # password=os.environ['PASSWORD2']
+    # print("host2",host)
     connection_pool = pooling.MySQLConnectionPool(pool_name="ws_pool",
                                                   pool_size=5,
                                                   host=host,
@@ -37,6 +37,44 @@ def validate():
     # password=os.environ['PASSWORD2']
    
     return "hello"
+
+
+@app.route('/users')
+def validate():
+    host='localhost'
+    database='furniture'
+    user='root'
+    password='Singapore1'
+    # host=os.environ['HOST2']
+    # database=os.environ['DATABASE2']
+    # user=os.environ['USERNAME2']
+    # password=os.environ['PASSWORD2']
+    # print("host2",host)
+    connection_pool = pooling.MySQLConnectionPool(pool_name="ws_pool",
+                                                  pool_size=5,
+                                                  host=host,
+                                                  database=database,
+                                                  user=user,
+                                                  password=password)
+    dbConn = connection_pool.get_connection()
+    cursor = dbConn.cursor(dictionary=True)
+    sql="select * from user"
+    cursor.execute(sql)
+    users = cursor.fetchall()
+
+    dbConn.close()
+    # host='localhost'
+    # database='furniture'
+    # user='root'
+    # password='Singapore1'
+
+    #Production
+    # host=os.environ['HOST2']
+    # database=os.environ['DATABASE2']
+    # user=os.environ['USERNAME2']
+    # password=os.environ['PASSWORD2']
+   
+    return "users"    
 
 
 
