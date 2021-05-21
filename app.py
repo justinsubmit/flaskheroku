159f81e3d5e83c25037e5f832cb5975fd14e2d71
justinsubmit <justinsubmit@gmail.com>
2021-05-21 17:28:39 +0800

    test

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,jsonify
-# from mysql.connector import pooling
-# from config.Settings import Settings
+from mysql.connector import pooling
+from config.Settings import Settings
 
 import os
 app = Flask(__name__)
@@ -13,20 +13,20 @@ def validate():
     return "hello world"
 
 
-# @app.route('/users')
-# def validate2():
-#     host='localhost'
-#     database='furniture'
-#     user='root'
-#     password='Singapore1'
-#     # host=os.environ['HOST2']
-#     # database=os.environ['DATABASE2']
-#     # user=os.environ['USERNAME2']
-#     # password=os.environ['PASSWORD2']
-#     host=os.environ['HOST']
-#     database=os.environ['DATABASE']
-#     user=os.environ['USERNAME']
-#     password=os.environ['PASSWORD']
+@app.route('/users')
+def validate2():
+    host='localhost'
+    database='furniture'
+    user='root'
+    password='Singapore1'
+    # host=os.environ['HOST2']
+    # database=os.environ['DATABASE2']
+    # user=os.environ['USERNAME2']
+    # password=os.environ['PASSWORD2']
+    host=os.environ['HOST']
+    database=os.environ['DATABASE']
+    user=os.environ['USERNAME']
+    password=os.environ['PASSWORD']
     # # print("host2",host)
 
     #    connection_pool = pooling.MySQLConnectionPool(pool_name="ws_pool",
@@ -39,53 +39,53 @@ def validate():
     # print("database", Settings.database)
     # print("password", Settings.password)
     # print("user", Settings.user)
-#     connection_pool = pooling.MySQLConnectionPool(pool_name="ws_pool",
-#                                                   pool_size=5,
-#                                                   host=host,
-#                                                   database=database,
-#                                                   user=user,
-#                                                   password=password)
-#     dbConn = connection_pool.get_connection()
-#     cursor = dbConn.cursor(dictionary=True)
-#     sql="select * from user"
-#     cursor.execute(sql)
-#     users = cursor.fetchall()
-#     try:
-#         jsonUsers=users
-#         jsonUsers={"Users":jsonUsers}
-#         return jsonify(jsonUsers)
-#     except Exception as err:
-#         print(err)
-#         return {},500
+    connection_pool = pooling.MySQLConnectionPool(pool_name="ws_pool",
+                                                  pool_size=5,
+                                                  host=host,
+                                                  database=database,
+                                                  user=user,
+                                                  password=password)
+    dbConn = connection_pool.get_connection()
+    cursor = dbConn.cursor(dictionary=True)
+    sql="select * from user"
+    cursor.execute(sql)
+    users = cursor.fetchall()
+    try:
+        jsonUsers=users
+        jsonUsers={"Users":jsonUsers}
+        return jsonify(jsonUsers)
+    except Exception as err:
+        print(err)
+        return {},500
 
-#     # dbConn.close()
-#     # host='localhost'
-#     # database='furniture'
-#     # user='root'
-#     # password='Singapore1'
+    # dbConn.close()
+    # host='localhost'
+    # database='furniture'
+    # user='root'
+    # password='Singapore1'
 
-#     #Production
-#     # host=os.environ['HOST2']
-#     # database=os.environ['DATABASE2']
-#     # user=os.environ['USERNAME2']
-#     # password=os.environ['PASSWORD2']
+    #Production
+    # host=os.environ['HOST2']
+    # database=os.environ['DATABASE2']
+    # user=os.environ['USERNAME2']
+    # password=os.environ['PASSWORD2']
    
-#     return "users"    
+    return "users"    
 
 
 
-# @app.route('/settings')
-# def Settings():
-#     # host='localhost'
-#     # database='furniture'
-#     # user='root'
-#     # password='Singapore1'
-#     host=os.environ['HOST']
-#     database=os.environ['DATABASE']
-#     user=os.environ['USERNAME']
-#     password=os.environ['PASSWORD']
-#     settings={"host":host,"username":user,"database":database}
-#     return jsonify(settings)
+@app.route('/settings')
+def Settings():
+    # host='localhost'
+    # database='furniture'
+    # user='root'
+    # password='Singapore1'
+    host=os.environ['HOST']
+    database=os.environ['DATABASE']
+    user=os.environ['USERNAME']
+    password=os.environ['PASSWORD']
+    settings={"host":host,"username":user,"database":database}
+    return jsonify(settings)
     
 
 if __name__ == '__main__':
